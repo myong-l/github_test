6a0e7318e4f216bc57e9ebb7f269bc7829d06dc5
myeongho lee <l.m.myeongho.lee@gmail.com>
2015-09-24 07:28:13 +0000

    Topページとお問い合わせページ作成ルーティング設定

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150924063429) do
+ActiveRecord::Schema.define(version: 20150924072452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20150924063429) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "statics", force: :cascade do |t|
+    t.string   "contact_title"
+    t.string   "contact_body"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
